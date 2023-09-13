2f3b18c7a88172bf477a380dd80640de220c5938
오창명 <136975040+kemarine@users.noreply.github.com>
2023-09-13 12:41:11 +0900

    Update PL 4.ml

--- PL/PL 4.ml
+++ PL/PL 4.ml
@@ -1,68 +1,68 @@
 (* insert item in back *)
-# let rec insert a l =
+let rec insert a l =
   match l with
-  | [] -> [a]
-  | hd::tl -> hd::(insert a tl);;
+    | [] -> [a]
+    | hd::tl -> hd::(insert a tl);;
 
 (* efficient way to reverse *)
-# let rec fast_rev l l' =
+let rec fast_rev l l' =
   match l with
-  | [] -> l'
-  | hd::tl -> fast_rev tl (hd::l');;
+    | [] -> l'
+    | hd::tl -> fast_rev tl (hd::l');;
 
-# let reverse l = fast_rev l [];;
+let reverse l = fast_rev l [];;
 
 (* remove all objects *)
-# let rec remove_all a l =
+let rec remove_all a l =
   match l with
-  | [] -> []
-  | hd::tl ->
-    if hd = a then (remove_all a tl)
-    else hd::(remove_all a tl);;
+    | [] -> []
+    | hd::tl ->
+      if hd = a then (remove_all a tl)
+      else hd::(remove_all a tl);;
 
 (* filter *)
-# let rec filter f l =
+let rec filter f l =
   match l with
-  | [] -> []
-  | hd::tl ->
-  if f hd then hd::(filter f tl)
-  else filter f tl;;
+    | [] -> []
+    | hd::tl ->
+      if f hd then hd::(filter f tl)
+      else filter f tl;;
 
 (* apply parameters *)
-# let even l = filter (fun x -> x mod 2 = 0) l;;
+let even l = filter (fun x -> x mod 2 = 0) l;;
 
-# let greater_than_five l = filter (fun x -> x > 5) l;;
+let greater_than_five l = filter (fun x -> x > 5) l;;
 
 (* fold-right *)
-# let rec fold_right f l a =
+let rec fold_right f l a =
   match l with
-  | [] -> a
-  | hd::tl -> f hd (fold_right f tl a);;
+    | [] -> a
+    | hd::tl -> f hd (fold_right f tl a);;
 
 (* apply parameters *)
-# let sum lst = fold_right (fun x y -> x + y) lst 0;;
-# let sum lst = List.fold_right (+) lst 0;;
+let sum lst = fold_right (fun x y -> x + y) lst 0;;
+let sum lst = List.fold_right (+) lst 0;;
 
-# let prod lst = fold_right (fun x y -> x * y) lst 1;;
-# let prod lst = List.fold_right ( *) lst 1;;
+let prod lst = fold_right (fun x y -> x * y) lst 1;;
+let prod lst = List.fold_right ( *) lst 1;;
 
 (* fold_left *)
 (* pattern analysis *)
 let rec sum a l =
   match l with
-  | [] -> a
-  | hd::tl -> sum (hd + a) tl;;
+    | [] -> a
+    | hd::tl -> sum (hd + a) tl;;
 
 let rec prod a l =
   match l with
-  | [] -> a
-  | hd::tl -> prod (hd * a) tl;;
+    | [] -> a
+    | hd::tl -> prod (hd * a) tl;;
 
 (* generalize *)
 let rec fold_left f a l =
   match l with
-  | [] -> a
-  | hd::tl -> fold_left (f a hd) tl;;
+    | [] -> a
+    | hd::tl -> fold_left (f a hd) tl;;
 
 (* length in fold_right *)
 let length l = fold_right (fun _ y -> y + 1) l 0;;
