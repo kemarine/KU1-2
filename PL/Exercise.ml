42bb96d37a14047681b568332cfd050eee64196f
오창명 <136975040+kemarine@users.noreply.github.com>
2023-09-14 14:09:04 +0900

    Update Exercise.ml

--- PL/Exercise.ml
+++ PL/Exercise.ml
@@ -1,8 +1,19 @@
 (* Problem 1 : fibonacci number *)
-# let rec fib x =
+let rec fib x =
   match x with
   | 0 -> 0
   | 1 -> 1
   | _ -> fib (x-1) + fib (x-2);;
 
-# let rec 
+(* Problem 2 : prime number *)
+let rec pri n x =
+  match n with
+  | 0 -> true
+  | 1 -> true
+  | _ ->
+  if x mod n = 0 then false
+  else pri (n-1) x;;
+
+let prime x = pri (x-1) x;;
+
+(* Problem 3 : binarize *)
