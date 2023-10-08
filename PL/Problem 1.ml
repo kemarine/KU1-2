0f9473dce69b233448266c5f2be92fbb8ab9f005
오창명 <136975040+kemarine@users.noreply.github.com>
2023-10-08 16:47:09 +0900

    Update Problem 1.ml

--- PL/Problem 1.ml
+++ PL/Problem 1.ml
@@ -1,3 +1,9 @@
+(* Do not open any module *)
+
+(***********************)
+(*  Library functions  *)
+(***********************)
+
 let rec fold_left : ('a -> 'b -> 'a) -> 'a -> 'b list -> 'a
 = fun f accu lst ->
   match lst with
@@ -10,4 +16,76 @@ let rec map : ('a -> 'b) -> 'a list -> 'b list
   | [] -> []
   | hd::tl -> (f hd)::(map f tl)
 
+(***********************)
+(******  Syntax  *******)
+(***********************)
 
+type program = exp
+and exp = 
+  | UNIT
+  | TRUE
+  | FALSE
+  | CONST of int
+  | VAR of var
+  | ADD of exp * exp
+  | SUB of exp * exp
+  | MUL of exp * exp
+  | DIV of exp * exp
+  | EQUAL of exp * exp
+  | LESS of exp * exp
+  | NOT of exp
+  | NIL
+  | CONS of exp * exp      
+  | APPEND of exp * exp
+  | HEAD of exp
+  | TAIL of exp
+  | ISNIL of exp
+  | IF of exp * exp * exp
+  | LET of var * exp * exp
+  | LETREC of var * var * exp * exp
+  | LETMREC of (var * var * exp) * (var * var * exp) * exp
+  | PROC of var * exp
+  | CALL of exp * exp
+  | PRINT of exp
+  | SEQ of exp * exp
+and var = string
+
+(***********************)
+(*****  Problem 1  *****)
+(***********************)
+
+type value = 
+  | Unit 
+  | Int of int 
+  | Bool of bool 
+  | List of value list
+  | Procedure of var * exp * env 
+  | RecProcedure of var * var * exp * env
+  | MRecProcedure of var * var * exp *
+                     var * var * exp * env
+and env = (var * value) list
+
+exception UndefinedSemantics
+
+let rec string_of_value v = 
+  match v with
+  | Int n -> string_of_int n
+  | Bool b -> string_of_bool b
+  | List lst -> "[" ^ fold_left (fun s x -> s ^ ", " ^ x) "" (map string_of_value lst) ^ "]"
+  | _ -> "(functional value)"
+
+let empty_env = []
+let extend_env (x,v) e = (x,v)::e
+let rec lookup_env x e = 
+  match e with
+  | [] -> raise (Failure ("variable " ^ x ^ " is not bound in env")) 
+  | (y,v)::tl -> if x = y then v else lookup_env x tl
+
+let rec eval : exp -> env -> value
+=fun exp env ->
+  match exp with
+  | PRINT e -> (print_endline (string_of_value (eval e env)); Unit)
+  | _ -> raise (Failure "Not implemented") (* TODO *)
+
+let runml : program -> value
+=fun pgm -> eval pgm empty_env
