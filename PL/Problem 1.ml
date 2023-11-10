645c9a1f7012ae4c80f588e979f751e9f5054944
Changmyeong Oh <136975040+kemarine@users.noreply.github.com>
2023-11-10 09:11:29 +0900

    Update Problem 1.ml

--- PL/Problem 1.ml
+++ PL/Problem 1.ml
@@ -92,51 +92,101 @@ let rec eval : exp -> env -> value
   | CONST n -> Int n
   | VAR v -> lookup_env v env
   | ADD (a, b) ->
-    let an = eval a env in
+    (let an = eval a env in
     let bn = eval b env in
-    (match an, bn with
+    match an, bn with  
     | Int an, Int bn -> Int (an + bn)
     | _ -> raise UndefinedSemantics)
   | SUB (a, b) ->
-    let an = eval a env in
+    (let an = eval a env in
     let bn = eval b env in
-    (match an, bn with
+    match an, bn with
     | Int an, Int bn -> Int (an - bn)
     | _ -> raise UndefinedSemantics)
   | MUL (a, b) ->
-    let an = eval a env in
+    (let an = eval a env in
     let bn = eval b env in
-    (match an, bn with
+    match an, bn with
     | Int an, Int bn -> Int (an * bn)
     | _ -> raise UndefinedSemantics)
   | DIV (a, b) ->
-    let an = eval a env in
+    (let an = eval a env in
     let bn = eval b env in
-    if bn = Int 0 then raise UndefinedSemantics
+    if bn = Int 0 then raise UndefinedSemantics (* ERROR NAME FIX *)
     else
-      (match an, bn with
+      match an, bn with
       | Int an, Int bn -> Int (an / bn)
       | _ -> raise UndefinedSemantics)
-  | EQUAL (a, b)
-    let an = eval a env in
+  | EQUAL (a, b) ->
+    (let an = eval a env in
     let bn = eval b env in
-    (match an, bn with
+    match an, bn with
     | Int an, Int bn -> Bool (an = bn)
     | Bool an, Bool bn -> Bool (an = bn)
     | _ -> raise UndefinedSemantics)
-  | LESS (a, b)
-    let an = eval a env in
+  | LESS (a, b) ->
+    (let an = eval a env in
     let bn = eval b env in
-    (match an, bn with
+    match an, bn with
     | Int an, Int bn -> Bool (an < bn)
     | _ -> raise UndefinedSemantics)
   | NOT e ->
     (match eval e env with
     | Bool x -> Bool (not x)
     | _ -> raise UndefinedSemantics)
-  | NIL (* START FROM HERE *)
-  | _ -> raise UndefinedSemantics
-
+  | NIL -> List []
+  | CONS (e1, e2) ->
+    (let e1 = eval e1 env in
+    let e2 = eval e2 env in
+    match e1, e2 with
+    | e1, List e2 -> List (e1::e2) (* del e1? or save it for type comp? *)
+    | _ -> raise UndefinedSemantics) (* add type comparison *)
+  | APPEND (e1, e2) ->
+    (let e1 = eval e1 env in
+    let e2 = eval e2 env in
+    match e1, e2 with
+    | List e1, List e2 -> List (e1 @ e2) (* add type comparison *)
+    | _ -> raise UndefinedSemantics)
+  | HEAD e ->
+    (let e = eval e env in
+      match e with
+    | List (hd::tl) -> hd
+    | _ -> raise UndefinedSemantics)
+  | TAIL e -> 
+    (let e = eval e env in
+    match e with
+    | List (hd::tl) -> List tl
+    | _ -> raise UndefinedSemantics)
+  | ISNIL e ->
+    (match eval e env with
+    | List [] -> Bool true
+    | _ -> Bool false)
+  | IF (e1, e2, e3) ->
+    (let e1 = eval e1 env in
+    match e1 with
+    | Bool x -> if x then eval e2 env else eval e3 env 
+    | _ -> raise UndefinedSemantics)
+  | LET (x, e1, e2) -> 
+    let e1 = eval e1 env in let e2 = eval e2 (extend_env (x, e1) env) in e2
+  | LETREC (f, x, e1, e2) ->
+    let e = RecProcedure (f, x, e1, env) in eval e2 (extend_env (f, e) env)
+  | PROC (x, e) -> Procedure (x, e, env)
+  | CALL (e1, e2) -> 
+    (let e1 = eval e1 env in
+    let v = eval e2 env in
+    match e1 with
+    | Procedure (x, e, env1) -> eval e (extend_env (x, v) env1)
+    | RecProcedure (f, x, e, env1) -> 
+      eval e (extend_env (x, v) (extend_env (f, e1) env1))
+    | MRecProcedure (f, x, ef, g, y, eg, env2) ->
+      eval ef (extend_env (g, MRecProcedure(g, y, eg, f, x, ef, env2)) (extend_env (f, e1) (extend_env (x, v) env2)))
+    | _ -> raise UndefinedSemantics)
+  | LETMREC ((f, x, e1), (g, y, e2), e3) ->
+    let env1 = extend_env (f, MRecProcedure (f, x, e1, g, y, e2, env)) env in
+    let env2 = extend_env (g, MRecProcedure (g, y, e2, f, x, e1, env)) env1 in
+    eval e3 env2
+  | SEQ (e1, e2) ->
+    let _ = eval e1 env in let e2 = eval e2 env in e2
   
 let runml : program -> value
-=fun pgm -> eval pgm empty_env
+=fun pgm -> eval pgm empty_env;;
